6fb72530bf33d295d3233b77b75c3e90e72c7e0a
Oscar Rodriguez <oskr226@gmail.com>
2022-07-14 12:06:09 -0500

    Creación del servicio ProductCategory en el cliente blazor. Implementación del httpClient para consumir el API.

---
+++ BlazorFornitureStore/Client/Services/IProductCategoryService.cs
@@ -0,0 +1,11 @@
+﻿using BlazorFornitureStore.Shared.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorFornitureStore.Client.Services
+{
+    public interface IProductCategoryService
+    {
+        Task<IEnumerable<ProductCategories>> GetProductCategories();
+    }
+}
---
+++ BlazorFornitureStore/Client/Services/ProductCategoryService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorFornitureStore.Shared.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazorFornitureStore.Client.Services
+{
+    public class ProductCategoryService : IProductCategoryService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductCategoryService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<ProductCategories>> GetProductCategories()
+        {
+            return await _httpClient.GetFromJsonAsync<IEnumerable<ProductCategories>>("api/productcategory");
+        }
+    }
+}
